04d2f45c317c66a35be39175af4afda7f9f4d1bd
Lavon <Lavon_Edwards@hotmail.com>
2020-12-24 03:34:38 -0600

    Did the Values and Variables Assignment
    Set up the assignment stuff, and Did the values and variables part of it. As I complete the lectures I will complete the assignments. Though they look simple enough for me to complete them right now, I want to see if there are anythings that I'm missing here and there. You never know I might learn an interesting new nugget here and there.

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,18 @@
+//////////////////////////////
+// assignment.js
+// PURPOSE: The practice assignment for JS fundamentals 1.
+// AUTHOR: Lavon Marquise Edwards
+// DATE:December 23, 2020
+// NOTES: 
+/////////////////////////////
+
+//  Lecture: Values and Variables
+let country = "Canada";
+let continent = "North America";
+let population = 37.59 * 1000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+//  Lecture Data Types
