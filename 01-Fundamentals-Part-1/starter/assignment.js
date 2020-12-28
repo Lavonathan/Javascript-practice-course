c0dccd5845c7c1f012b3861b356034f1949634ca
Lavon <Lavon_Edwards@hotmail.com>
2020-12-28 01:11:49 -0600

    Completed Assignment for lecture 14

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -9,7 +9,7 @@
 //  Lecture: Values and Variables
 //let country = "Canada";
 //let continent = "North America";
-let population = 37.59 * 1000000;
+let canadaPopulation = 37.59 * 1000000;
 
 
 
@@ -18,7 +18,7 @@ let isIsland = false;
 let language;
 
 console.log('isIsland: ' + isIsland);
-console.log('population: ' + population);
+console.log('population: ' + canadaPopulation);
 
 //  Lecture let, const, and var
 language = "english";
@@ -29,3 +29,22 @@ const continent = "North America";
 
 console.log('Country: ' + country);
 console.log('Continent: ' + continent);
+
+//////////////////////////////////////////////
+//  Lecture Basic Operators.
+const halfPopulation = canadaPopulation / 2;
+canadaPopulation++;
+console.log("population: " + canadaPopulation)
+
+const finlandPopulation = 6 * 1000000;
+console.log(canadaPopulation > finlandPopulation)
+
+const averageCountryPopulation = 33 * 1000000;
+console.log(canadaPopulation < averageCountryPopulation);
+
+let description = country + "is in " + continent + ", and its " + canadaPopulation + " million people speak " + language;
+
+console.log(description);
+
+//////////////////////////////////////////////
+//  Lecture ....
